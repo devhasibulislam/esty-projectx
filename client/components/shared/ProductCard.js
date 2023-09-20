033cd3aae50264f0898dc289f0bd9045b5f8a356
devhasibulislam <devhasibulislam@gmail.com>
2023-09-20 13:34:36 +0600

    integrate modal component

--- client/components/shared/ProductCard.js
+++ client/components/shared/ProductCard.js
@@ -14,10 +14,20 @@
  */
 
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import Modal from "./Modal";
 
 const ProductCard = ({ product }) => {
   const { title, price, shop, thumbnail } = product || {};
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <div className="flex flex-col gap-y-4">
@@ -40,10 +50,67 @@ const ProductCard = ({ product }) => {
             {shop}
           </p>
         </div>
-        <button className="bg-violet-500 hover:bg-violet-700 transition-colors delay-100 py-2 px-4 rounded-r-full text-white w-fit text-sm">
+        <button
+          onClick={openModal}
+          className="bg-slate-300 hover:bg-slate-500 transition-colors delay-100 py-2 px-4 rounded-r-full w-fit text-sm"
+        >
           Buy Now
         </button>
       </article>
+
+      {isModalOpen && (
+        <Modal
+          isOpen={isModalOpen}
+          onClose={closeModal}
+          title="Manage Your Cart"
+        >
+          <div className="h-full w-full flex gap-x-2">
+            <Image
+              src={thumbnail}
+              alt={title}
+              height={214}
+              width={214}
+              className="rounded h-[214px] object-cover object-top"
+            />
+            <div className="flex flex-col gap-y-2">
+              <div className="bg-[#eaeaea99] p-2 rounded flex flex-col gap-y-2 h-fit">
+                <h2 className="line-clamp-2">{title}</h2>
+                <div className="flex flex-col gap-y-0">
+                  <p className="text-base flex flex-row gap-x-0.5 items-baseline pb-0">
+                    <span className="text-xs">Price: $</span>
+                    {price}
+                  </p>
+                  <p className="text-base flex flex-row gap-x-0.5 items-baseline pb-0">
+                    <span className="text-xs">Shop from</span>
+                    {shop}
+                  </p>
+                </div>
+              </div>
+
+              <div className="bg-[#eaeaea99] p-2 rounded flex flex-col gap-y-2 h-fit">
+                <div className="flex flex-col gap-y-0">
+                  <p className="text-base flex flex-row gap-x-0.5 items-baseline pb-0">
+                    <span className="text-xs">Shipping: $</span>
+                    {(price * 0.2).toFixed(2)}
+                  </p>
+                  <p className="text-base flex flex-row gap-x-0.5 items-baseline pb-0">
+                    <span className="text-xs">Shop from:</span>
+                    {shop}
+                  </p>
+                  <p className="text-base flex flex-row gap-x-0.5 items-baseline pb-0">
+                    <span className="text-xs">Estimated delivery:</span>
+                    {shop}
+                  </p>
+                </div>
+              </div>
+
+              <button className="rounded py-0.5 bg-slate-300 hover:bg-slate-500 transition-colors delay-100">
+                Checkout
+              </button>
+            </div>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
