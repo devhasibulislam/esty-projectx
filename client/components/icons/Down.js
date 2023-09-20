1fc6a658bda0c1232d48b82c7459735a29ae16e9
devhasibulislam <devhasibulislam@gmail.com>
2023-09-20 08:06:15 +0600

    init down arrow icon

---
+++ client/components/icons/Down.js
@@ -0,0 +1,32 @@
+/**
+ * Title: Write a program using JavaScript on Down
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 19, September 2023
+ */
+
+import React from "react";
+
+const Down = (porps) => {
+  return (
+    <svg
+      {...porps}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      aria-hidden="true"
+      focusable="false"
+    >
+      <polygon points="16.5 10 12 16 7.5 10 16.5 10"></polygon>
+    </svg>
+  );
+};
+
+export default Down;
