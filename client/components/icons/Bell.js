b55b6765d089438521e2439524fd9fa18e13f166
devhasibulislam <devhasibulislam@gmail.com>
2023-09-20 08:06:02 +0600

    init bell icon

---
+++ client/components/icons/Bell.js
@@ -0,0 +1,32 @@
+/**
+ * Title: Write a program using JavaScript on Bell
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 19, September 2023
+ */
+
+import React from "react";
+
+const Bell = (props) => {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      aria-hidden="true"
+      focusable="false"
+    >
+      <path d="M20.8,18.4C19.3,16.7,19,12.5,19,11c0-3.2-2.1-5.8-5-6.7V4c0-1.1-0.9-2-2-2s-2,0.9-2,2l0,0v0.3C7.1,5.2,5,7.8,5,11 c0,1.5-0.3,5.7-1.8,7.4c-0.3,0.3-0.3,0.7-0.1,1.1S3.6,20,4,20h4.6c0.7,1.2,2,2,3.4,2s2.8-0.8,3.4-2H20c0.4,0,0.7-0.2,0.9-0.6 S21,18.6,20.8,18.4z M16,18H8H5.8C7,15.3,7,11.5,7,11c0-2.8,2.2-5,5-5s5,2.2,5,5c0,0.5,0,4.3,1.2,7H16z"></path>
+    </svg>
+  );
+};
+
+export default Bell;
