eb076ef61ceaf0c5fb0d933ac598f3a87350e194
devhasibulislam <devhasibulislam@gmail.com>
2023-09-20 09:10:54 +0600

    init outside click component

---
+++ client/components/shared/OutsideClickClose.js
@@ -0,0 +1,37 @@
+/**
+ * Title: Write a program using JavaScript on OutsideClickClose
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 20, September 2023
+ */
+
+import React, { useEffect, useRef } from "react";
+
+const OutsideClickClose = () => {
+  const wrapperRef = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+        onOutsideClick();
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [onOutsideClick]);
+
+  return <div ref={wrapperRef}>{children}</div>;
+};
+
+export default OutsideClickClose;
