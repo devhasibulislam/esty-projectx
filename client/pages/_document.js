85044111592192f26600e7dd5d2c9a11d1a36cbb
devhasibulislam <devhasibulislam@gmail.com>
2023-10-01 00:59:40 +0600

    init initial meta data

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -4,6 +4,19 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta title="Etsy ProjectX" name="title" />
+
+        <meta name="description" content="Etsy ProjectX as Template" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://projectx.etsy.com" />
+        <meta property="og:title" content="Etsy ProjectX" />
+        <meta property="og:description" content="Etsy ProjectX as Template" />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dkmf4t55r/image/upload/v1693567142/Og-Images/devhasibulislam.png"
+        />
+
         <link
           rel="apple-touch-icon"
           sizes="180x180"
